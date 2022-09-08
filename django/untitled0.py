5d57cd517c9c122d2a984fdc0c6905750c4a2558
Ziyodu11o <muxtorovziyodullo91@gmail.com>
2022-09-08 20:49:40 +0500

    first

---
+++ django/untitled0.py
@@ -0,0 +1,29 @@
+str='bugun online dars'
+
+#if str == "bugun online dars":
+#    print("mavjud")
+    
+#else:
+#    print("mavjud emas")
+    
+    
+#print(str.replace('b','B'))
+
+#a=2
+#b=4
+
+#print(a is b)
+
+
+#a=bool(1)
+#b=bool(0)
+
+#print(a,b)
+
+if False==1:
+    print("rost")
+    
+else:
+    print("yolgon")
+    
+
